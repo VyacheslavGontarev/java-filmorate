1e4791329cb5a37f37ea1237e2d5d3d0b192dd30
Vyacheslav Gontarev <vyacheslav.gontarev@yandex.ru>
2025-03-12 22:48:50 +0300

    Закончена реализация согласно ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import java.time.Duration;
+import java.time.LocalDate;
+import ru.yandex.practicum.filmorate.controller.serializer.DurationSerializer;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    @JsonSerialize(using = DurationSerializer.class)
+    Duration duration;
+
+    public Film(Long id, String name, String description, LocalDate releaseDate, Duration duration) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
